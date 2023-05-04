beca9cce52e452e5ee2759613f4748b8f4490147
KoshmareG <koshmareg@yandex.ru>
2023-05-04 22:36:02 +0700

    fix: fix problem with undefined username symbol

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         authentication_keys: :username
+         :authentication_keys => [:username]
 
   has_many :records, dependent: :destroy
 
